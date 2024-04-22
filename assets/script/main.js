622dce28787001081ce957ff71a4592594efecc0
mayu.hiruta <k.khg42.206@gmail.com>
2024-04-22 22:01:09 +0900

    修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -22,36 +22,6 @@ $(function () {
   });
 });
 
-// リンクに対するマウスオーバー時のアニメーション
-var onMouseenter = function (e) {
-  $(e.target).css({
-    'opacity': '0.6',
-  });
-};
-var onMouseleave = function (e) {
-  $(e.target).css({
-    'opacity': '1',
-  });
-};
-$('a:not(a.no-hover)')
-  .on('mouseenter', onMouseenter)
-  .on('mouseleave', onMouseleave);
-
-  if($(window).innerWidth() <= 767){
-  $('.transform').on('mouseenter', function (e) {
-    $(this).css({ 'transform': 'unset' });
-  });
-}else{
-  $('.transform').on('mouseenter', function (e) {
-    $(this).css({ 'transform': 'scale(1.2,1.2)' });
-  });
-  $('.transform').on('mouseleave', function (e) {
-    $(this).css({ 'transform': 'unset' });
-  })};
-
-
-
-
   // タブ
   $('#smarttab').smartTab({
     enableUrlHash: false
